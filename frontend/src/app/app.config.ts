1ffba468ec326a7167cd3917cb97e15593f2600f
Felipe Figueiredo <fjnfigueiredo@gmail.com>
2025-07-23 09:43:12 -0300

    chore: mark stable build with working backend and Angular setup

---
+++ frontend/src/app/app.config.ts
@@ -0,0 +1,21 @@
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+
+// Here you will import the reducers and effects in the future
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes),
+    provideStore({}), // <- Here we place reducers in the future
+    provideEffects([]), // <- Here we place the Effects
+    provideStoreDevtools({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
+  ],
+};
